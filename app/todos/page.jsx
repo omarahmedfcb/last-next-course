6f5371c670291466e892271fca4b39c5992e7d1f
Omar Ahmed <manoomarfcb2000@gmail.com>
2025-10-14 15:48:50 +0300

    finished 2

--- app/todos/page.jsx
+++ app/todos/page.jsx
@@ -7,9 +7,11 @@ async function Todos() {
   const data = await req.json();
   return (
     <div className=" flex flex-col items-stretch gap-8 w-7/10 mx-auto">
-      {data.todos.map((ele) => (
-        <TodoCard key={ele._id} todo={ele} />
-      ))}
+      {data?.todos?.length > 0 ? (
+        data.todos.map((ele) => <TodoCard key={ele._id} todo={ele} />)
+      ) : (
+        <p>No todos found.</p>
+      )}
     </div>
   );
 }
